e151a95101451f57236cec824968ff6fd87488e8
Daniel Augusto Bender <danielaugustob569@gmail.com>
2026-05-06 17:07:04 -0300

    numero alterado
    numeroalterado

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function comprar(produto) {
-  let numero = "5599999999999"; // número da academia
+  let numero = "5549992025959"; // seu número
   let mensagem = `Olá, tenho interesse no ${produto}`;
   let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
   
