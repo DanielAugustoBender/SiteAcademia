7c3854fe97f8d85ee2001351259ceb407eebdb58
Daniel Augusto Bender <danielaugustob569@gmail.com>
2026-05-07 12:48:36 -0300

    Refinamento
    -melhoria do css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function comprar(produto) {
   let numero = "5549992025959"; // seu número
-  let mensagem = `Olá, tenho interesse no ${produto}`;
+  let mensagem = `Olá! Vi o produto ${produto} no site da Gym Complex e gostaria de mais informações.`;
   let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
   
   window.open(url, "_blank");
